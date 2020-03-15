7b96eb7996e6a26621fc46e9764edcb9f2fe5ad9
Caleb <CalebBabin@users.noreply.github.com>
2020-03-15 12:39:37 -0500

    format js

--- gif-server.js
+++ gif-server.js
@@ -18,10 +18,10 @@ if (!fs.existsSync(`${__dirname}/gifs`)) {
 
 app.use('/static', express.static(path.join(__dirname, 'gifs')));
 
-app.get('/gif/:id', (req, res)=>{
+app.get('/gif/:id', (req, res) => {
     const id = req.params.id.replace(/\W/g, '');
     const dir = `${__dirname}/gifs/${id}`;
-	
+
     if (fs.existsSync(dir)) {
         const files = fs.readdirSync(dir);
 
@@ -39,33 +39,33 @@ app.get('/gif/:id', (req, res)=>{
     } else {
         const fileDir = `${__dirname}/gifs/${id}.gif`;
         const file = fs.createWriteStream(fileDir);
-        const request = https.get(`https://cdn.betterttv.net/emote/${id}/3x`, function(response) {
+        const request = https.get(`https://cdn.betterttv.net/emote/${id}/3x`, function (response) {
             response.on('end', () => {
                 if (!fs.existsSync(dir)) {
-                fs.mkdirSync(dir)
-                extractFrames({
-                    input: fileDir,
-                    output: dir+'/%d.png',
-                    coalesce: false,
-                })
-                .then((data)=>{
-                    const json = JSON.stringify({
-                        count: data.shape[0],
-                        id: id,
-                        cacheHit: false
-                    });
-                    res.send(json);
-                    
-                })
-                .catch(err => {
-                    res.json({
-                        count: 0,
-                        error: "Error extracting GIF"
-                    });
-                    fs.writeFileSync(`${__dirname}/gifs/${id}.exists`, '');
-                    fs.unlink(fileDir);
-                    fs.rmdirSync(dir);
-                })
+                    fs.mkdirSync(dir)
+                    extractFrames({
+                        input: fileDir,
+                        output: dir + '/%d.png',
+                        coalesce: false,
+                    })
+                        .then((data) => {
+                            const json = JSON.stringify({
+                                count: data.shape[0],
+                                id: id,
+                                cacheHit: false
+                            });
+                            res.send(json);
+
+                        })
+                        .catch(err => {
+                            res.json({
+                                count: 0,
+                                error: "Error extracting GIF"
+                            });
+                            fs.writeFileSync(`${__dirname}/gifs/${id}.exists`, '');
+                            fs.unlink(fileDir);
+                            fs.rmdirSync(dir);
+                        })
                 }
             });
             response.pipe(file);
@@ -74,7 +74,7 @@ app.get('/gif/:id', (req, res)=>{
 })
 
 app.use((req, res, next) => {
-	res.status(404).send('404');
+    res.status(404).send('404');
 })
 
 app.listen(42069);
