15eb4cedccb483ae4d59b0354b18e5962fe3a3f2
Caleb <CalebBabin@users.noreply.github.com>
2020-05-03 15:34:30 -0500

    add bttv endpoint, fuck you night, stop deprecating shit you little bitch

--- gif-server.js
+++ gif-server.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const fetch = require('node-fetch');
 
 const tryGettingFile = require('./utils/download.js');
 
@@ -61,6 +62,81 @@ app.get('/gif/:id', (req, res) => {
         });
     }
 })
+
+app.get('/channel/username/:username', (req, res) => {
+    const filtered_username = req.params.username.replace( /[^a-zA-Z0-9]/ , "");
+    fetch("https://api.twitch.tv/helix/users?login="+filtered_username, {
+        headers: {
+            "Client-ID": "6ro4h73lmdtiaxzhq6t5c7fr9ix50r"
+        }
+    })
+    .then(response => response.json())
+    .then(body => {
+        if (body && body.data && body.data[0] && body.data[0].id) {
+            getBTTVEmotes(body.data[0].id)
+            .then(data => {
+                res.set('Cache-Control', 'max-age=3600')
+                res.json(data);
+            })
+            .catch(e => {
+                console.error(e);
+                res.json({
+                    error: e
+                });
+            })
+
+            
+        } else {
+            res.json({
+                error: "something went wrong getting the ID"
+            })
+        }
+    })
+    .catch(e => {
+        console.error(e);
+        res.json({
+            error: "something went wrong fetching from the twitch API"
+        })
+    })
+})
+
+const getBTTVEmotes = (id) => {
+    return new Promise((resolve, reject) => {
+        
+        fetch('https://api.betterttv.net/3/cached/emotes/global')
+        .then(json => json.json())
+        .then(global_emotes_body => {
+            const output = [];
+
+            if (global_emotes_body && global_emotes_body.length > 0) {
+                for (let index = 0; index < global_emotes_body.length; index++) {
+                    const emote = global_emotes_body[index];
+                    output.push(emote);
+                }
+            }
+            
+            fetch(`https://api.betterttv.net/3/cached/users/twitch/${id}`)
+            .then(response => response.json())
+            .then(channel_emotes_body => {
+                
+                if (channel_emotes_body.channelEmotes) {
+                    for (let index = 0; index < channel_emotes_body.channelEmotes.length; index++) {
+                        const emote = channel_emotes_body.channelEmotes[index];
+                        output.push(emote);
+                    }
+                }
+                if (channel_emotes_body.sharedEmotes) {
+                    for (let index = 0; index < channel_emotes_body.sharedEmotes.length; index++) {
+                        const emote = channel_emotes_body.sharedEmotes[index];
+                        output.push(emote);
+                    }
+                }
+
+                resolve(output);
+            }).catch(e => {reject(e)})
+        }).catch(e => {reject(e)})
+    })
+}
 
 
 app.use((req, res, next) => {
